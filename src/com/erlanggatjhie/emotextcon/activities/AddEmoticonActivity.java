523d77616e6102bfd5aaaba371c318660b8123e6
erlanggat <erlangga_tjhie@hotmail.com>
2013-02-17 21:51:48 +1100

    Added check when there is no content provided

--- src/com/erlanggatjhie/emotextcon/activities/AddEmoticonActivity.java
+++ src/com/erlanggatjhie/emotextcon/activities/AddEmoticonActivity.java
@@ -2,11 +2,30 @@ package com.erlanggatjhie.emotextcon.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddEmoticonActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_emoticon_activity);
+		initialiseComponent();
+	}
+
+	private void initialiseComponent() {
+		Button addButton = (Button) findViewById(R.id.addEmoticonButton);
+		final EditText contentTextView = (EditText) findViewById(R.id.contentAddEmoticonEditText);
+		addButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if ("".equals(contentTextView.getText().toString().trim())) {
+					
+					Toast.makeText(AddEmoticonActivity.this, getResources().getString(R.string.no_content_error_message), Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
 	}
 }
